5712fd4c94e43a966c0ff9213158ea457391c644
Manuel Fiallos <contactofiallosorlando@gmail.com>
2022-07-20 09:11:27 -0600

    Hash router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Header";
 import Products from "./Products";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Product from "./Components/Product";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Routes>
+      <HashRouter>
         <Route path="/" element={<Products />} />
         <Route path="/product/:id" element={<Product />} />
-      </Routes>
+      </HashRouter>
       <Footer />
     </div>
   );
